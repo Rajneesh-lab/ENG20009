80b5ebb7c7f22da9be20b00562e5a9a687be0b00
HamishM58 <hamishfmitchell@gmail.com>
2025-04-26 18:36:02 +1000

    Update ProjectCode.ino
    Changed Setup. Removed header file
    
    
    Signed-off-by: HamishM58 <hamishfmitchell@gmail.com>

--- ProjectCode.ino
+++ ProjectCode.ino
@@ -1,58 +1,113 @@
-//Setup & Defining functions
+
+//RTC
+#include <RTClib.h>
+RTC_DS1307 rtc;
+DateTime now;
+
+//IMU
+#include <DFRobot_BMX160.h>
+DFRobot_BMX160 IMU;
+sBmx160SensorData_t Omagn, Ogyro, Oaccel;
+
+//BH1750
+#include <hp_BH1750.h>
+hp_BH1750 DLDR;
+
+//BME BME680
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME680.h>
+#define SEALEVELPRESSURE_HPA (1013.25);
+Adafruit_BME680 AQS;
+
+//Button Pin allocation:
 void DefineButtons(){
   pinMode(2, INPUT_PULLUP);
   pinMode(3, INPUT_PULLUP);
   pinMode(4, INPUT_PULLUP);
   pinMode(5, INPUT_PULLUP);
-  Serial.println("Buttons Defined");
+  Serial.println("BUTTON PINS ALLOCATED");
 }
 
-//Header File with Eng_Sensor class
-#include "ENG_Sensors.h"
+  
+  
 
-void setup() {
+bool SetUpErrorFlag = false;
+
+void setup(){
   Serial.begin(9600);
-  ENG_Sensors Sensors; //Must be initialised after Serial.begin and in SetUP()
+  Serial.println();
+  Serial.println("--STARTING SENSOR COMMS ON I2C--");
+  if (rtc.begin() == false) {
+    Serial.println("RTC NOT CONNECTED");
+    SetUpErrorFlag = true;
+  }
+  else{
+    Serial.println("RTC CONNECTED");
+    rtc.adjust(DateTime(2025,4,26,0,0,0));
+  }
+  if (IMU.begin() == false) {  
+    Serial.println("IMU NOT CONNECTED");    
+    SetUpErrorFlag = true;
+  }
+  else{
+    Serial.println("IMU CONNECTED");
+  }
+  if (DLDR.begin(BH1750_TO_GROUND) == false) {
+    Serial.println("DLDR NOT CONNECTED");
+    SetUpErrorFlag = true;
+  }
+  else{
+    Serial.println("DLDR CONNECTED");
+  }
+  if (AQS.begin(0x76) == false) {
+    Serial.println("AQS NOT CONNECTED");
+    SetUpErrorFlag = true;
+  }
+  else{
+    Serial.println("AQS CONNECTED");
+    AQS.setTemperatureOversampling(BME680_OS_8X);
+    AQS.setHumidityOversampling(BME680_OS_2X);
+    AQS.setPressureOversampling(BME680_OS_4X);
+    AQS.setIIRFilterSize(BME680_FILTER_SIZE_3);
+    AQS.setGasHeater(320, 150);  // 320*C for 150 ms
+  }
   DefineButtons();
+  while(SetUpErrorFlag == true){
+    Serial.println("Setup Error");
+    delay(1000);
+  }
 }
-void loop() {
+
+
+void loop(){
+  now =rtc.now();
+  IMU.getAllData(&Omagn, &Ogyro, &Oaccel);
   if(ButtonPressed(1)){
     Serial.println("You pressed button 1");
   }
-
 }
 //Functions
+
 //Function that checks if a button is pressed and released before returning True. buttonN corrosponds to button 1-4 on the board.
+//BLOCKING
 bool ButtonPressed(int buttonN){
-  int buttonNf;
-  switch(buttonN){
-  case 1:
-  buttonNf = 2;
-  break;
-  case 2:
-  buttonNf = 3;
-  break;
-  case 3:
-  buttonNf = 4;
-  break;
-  case 4:
-  buttonNf = 5;
-  break;  
-  default:
-  buttonNf =0;
-  Serial.println("Button assignment error, No button:" + String(buttonN));
-  break;
+  buttonN++;
+  if(buttonN>5 || buttonN<2){
+    Serial.print("ButtonError Out of range");
+    return false;
   }
-  if((digitalRead(buttonNf)==LOW) && (buttonNf != 0)){
+  else{
+    if((digitalRead(buttonN)==LOW) && (buttonN != 0)){
     while(true){
       //Serial.println("button " + String(buttonNf) + " pressed");
-      if(digitalRead(buttonNf)==HIGH){
+      if(digitalRead(buttonN)==HIGH){
         //Serial.println("button " + String(buttonNf) + " released");
         return true;
       }
     }
-  }
-  else{
+    }
+    else{
     return false;
+    }
   }
 }
