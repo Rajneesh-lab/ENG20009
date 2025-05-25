0d6e298905ed24bb840e3a0af7540acec0019308
HamishM58 <hamishfmitchell@gmail.com>
2025-05-25 19:43:25 +1000

    Update ProjectCode.ino
    Finalized Sensor functions and ensured all sensors can be accessed without breaking I2C bus
    
    
    Signed-off-by: HamishM58 <hamishfmitchell@gmail.com>

--- ProjectCode.ino
+++ ProjectCode.ino
@@ -1,8 +1,22 @@
 //Scanner for diagnostics
 #include "I2CScanner.h"
 I2CScanner scanner;
-  //scanner.Init();
-  //scanner.Scan();
+
+//LCD PINS
+#define TFT_CS    10
+#define TFT_RST   6 
+#define TFT_DC    7 
+#define TFT_SCLK 13   
+#define TFT_MOSI 11   
+//Adafruit_ST7735 LCD SETUP
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include <SPI.h>
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+
+
+//I2C bus
+#include <Wire.h>
 
 //RTC
 #include <RTClib.h>
@@ -14,8 +28,8 @@ DateTime now;
 DFRobot_BMX160 IMU;
 sBmx160SensorData_t Omagn, Ogyro, Oaccel;
 
-//BH1750
-#include <hp_BH1750.h>
+//BH1750 Christopher Laws
+#include <hp_BH1750.h> 
 hp_BH1750 DLDR;
 
 //BME BME280
@@ -33,11 +47,14 @@ void DefineButtons(){
 }
 
   
-int DIRO = 7;//pin that controls the input/output state SDI adaptor. HIGH receives and low sends from arduino
+int DIRO = 9;//pin that controls the input/output state SDI adaptor. HIGH receives and low sends from arduino
 
 bool SetUpErrorFlag = false;
 
 void setup(){
+  Serial.begin(9600);
+  Wire.begin();
+  SetupLCD();
   SetupSensors();
   SetupSDI12();
   DefineButtons();
@@ -45,11 +62,8 @@ void setup(){
 
 
 void loop(){
-  now =rtc.now();
-  IMU.getAllData(&Omagn, &Ogyro, &Oaccel);
   if(ButtonPressed(1)){
-    Serial.println("You pressed button 1");
-    SDI12send("abc");
+    ReadAll();
   }
   if(ButtonPressed(2)){
     Serial.println("You pressed button 2");
@@ -59,7 +73,7 @@ void loop(){
 //Functions
 
 //Function that checks if a button is pressed and released before returning True. buttonN corrosponds to button 1-4 on the board.
-//BLOCKING
+//BLOCKING!!
 bool ButtonPressed(int buttonN){
   buttonN++;
   if(buttonN>5 || buttonN<2){
@@ -101,7 +115,6 @@ String SDI12receive(){
 }
 void SetupSensors(){
   scanner.Init();
-  Serial.begin(9600);
   Serial.println();
   Serial.println("--STARTING SENSOR COMMS ON I2C--");
   if (rtc.begin() == false) {
@@ -137,10 +150,113 @@ void SetupSensors(){
     scanner.Scan(); //Debugging Loop
     delay(1000);
   }
+
 }
+
+//BME Enviromental sensor
+float ReadTemperature(){
+  return BME.readTemperature();
+}
+float ReadPressure(){
+  return (BME.readPressure() / 100.0F);
+}
+float ReadAltitude(){
+  return BME.readAltitude(SeaLevel_HPA);
+}
+float ReadHumidity(){
+  return BME.readHumidity();
+}
+
+//Acceleration
+float ReadAccelerationX(){
+  IMU.getAllData(&Omagn, &Ogyro, &Oaccel);
+  return Oaccel.x;
+}
+float ReadAccelerationY(){
+  IMU.getAllData(&Omagn, &Ogyro, &Oaccel);
+  return Oaccel.y;
+}
+float ReadAccelerationZ(){
+  IMU.getAllData(&Omagn, &Ogyro, &Oaccel);
+  return Oaccel.z;
+}
+//Magnetometer
+float ReadMagnetometerX(){
+  IMU.getAllData(&Omagn, &Ogyro, &Oaccel);
+  return Omagn.x;
+}
+float ReadMagnetometerY(){
+  IMU.getAllData(&Omagn, &Ogyro, &Oaccel);
+  return Omagn.y;
+}
+float ReadMagnetometerZ(){
+  IMU.getAllData(&Omagn, &Ogyro, &Oaccel);
+  return Omagn.z;
+}
+//gyrometer 
+float ReadGyrometerX(){
+  IMU.getAllData(&Omagn, &Ogyro, &Oaccel);
+  return Ogyro.x;
+}
+float ReadGyrometerY(){
+  IMU.getAllData(&Omagn, &Ogyro, &Oaccel);
+  return Ogyro.y;
+}
+float ReadGyrometerZ(){
+  IMU.getAllData(&Omagn, &Ogyro, &Oaccel);
+  return Ogyro.z;
+}
+
+//Light Sensor
+float ReadLux(){
+  DLDR.start();
+  //Wait for measurement to be ready
+  while(!DLDR.hasValue()){
+  }
+  return DLDR.getLux();
+}
+
+//Diagnostic to check all sensors
+void ReadAll(){
+  Serial.println("###BME###");
+  delay(300);
+  Serial.println("ReadTemperature " + String(ReadTemperature()));
+  delay(300);
+  Serial.println("ReadPressure " + String(ReadPressure()));
+  delay(300);
+  Serial.println("ReadAltitude " + String(ReadAltitude()));
+  delay(300);
+  Serial.println("ReadHumidity " + String(ReadHumidity()));
+
+  Serial.println("###IMU###");
+  delay(300);
+  Serial.println("ReadAccelerationX " + String(ReadAccelerationX()));
+  Serial.println("ReadAccelerationY " + String(ReadAccelerationY()));
+  Serial.println("ReadAccelerationZ " + String(ReadAccelerationZ()));
+  delay(300);
+  Serial.println("ReadMagnetometerX " + String(ReadMagnetometerX()));
+  Serial.println("ReadMagnetometerY " + String(ReadMagnetometerY()));
+  Serial.println("ReadMagnetometerZ " + String(ReadMagnetometerZ()));
+  delay(300);
+  Serial.println("ReadGyrometerX " + String(ReadGyrometerX()));
+  Serial.println("ReadGyrometerY " + String(ReadGyrometerY()));
+  Serial.println("ReadGyrometerZ " + String(ReadGyrometerZ()));
+
+
+  Serial.println("###DLDR###");
+  Serial.println("DLDR " + String(ReadLux()));
+}
+
 void SetupSDI12(){
   //SDI-12
   Serial1.begin(1200, SERIAL_7E1);//SDI-12 UART, configures serial port for 7 data bits, even parity, and 1 stop bit
   pinMode(DIRO, OUTPUT);//DIRO Pin
   digitalWrite(DIRO, HIGH);//lsiten
+}
+
+void SetupLCD(){
+  tft.initR(INITR_BLACKTAB);
+  tft.setRotation(3);
+  tft.fillScreen(ST77XX_BLACK);
+  Serial.print("LCD SetUp");
 }
